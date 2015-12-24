5e6f168008786b15e45ed9e122d3a5f4c8e1aad4
boarnoah <adipamihi@gmail.com>
2015-12-24 16:25:24 -0500

    Cleaned GameScene

--- src/GameScene.hx
+++ src/GameScene.hx
@@ -1,12 +1,14 @@
 package;
-import com.haxepunk.graphics.Text;
-import com.haxepunk.Scene;
-import entities.GuiManager;
-
-import com.haxepunk.utils.Input;
 
 import com.haxepunk.HXP;
+import com.haxepunk.graphics.Text;
+import com.haxepunk.Scene;
+import com.haxepunk.utils.Input;
 
+import GScene;
+import GNavigation;
+
+import entities.GuiManager;
 import entities.GMap;
 import entities.EnemyManager;
 import entities.InputManager;
@@ -16,23 +18,28 @@ import entities.InputManager;
  * @author Emseers
  */
 class GameScene extends Scene{
-	//public var gameMapData:TmxMap;
-	//public var gameMap:TmxEntity;
 	public var currentMapName:String;
+	
+	//Haxe Classes
+	public var gameNav:GNavigation;
+	
+	//Haxepunk Entities
 	public var gameMap:GMap;
 	public var enemyManager:EnemyManager;
 	public var inputManager:InputManager;
 	public var guiManager:GuiManager;
 	
-	var tileX = 0;
-	var tileY = 0;
+	var tileX:Int = 0;
+	var tileY:Int = 0;
 	var textCoord = new Text("( x, y)");
-	
-	
 	
 	public function new() {
 		super();
-		currentMapName = "maps/" + "tMap3" + ".tmx";
+		currentMapName = "maps/" + "tMap6" + ".tmx";
+		
+		gameNav = new GNavigation();
+		
+		// Game Entities
 		gameMap = new GMap();
 		enemyManager = new EnemyManager();
 		inputManager = new InputManager();
@@ -44,18 +51,17 @@ class GameScene extends Scene{
 		add(guiManager);	
 	}
 	
-	public override function begin() {
+	override public function begin() {
+		super.begin();
+		
+		// A begin function for gameEntities (begin is called after all entities are loaded in)
+		gameMap.begin();
 		inputManager.begin();
 		guiManager.begin();
 		
 		gameMap.loadMap(currentMapName);
 		guiManager.loadGUI();
-		
-		//trace("WIDTH : " + Main.tW);
-		//gameMap.getObjects("oLayer1");
 		inputManager.setCameraBounds(gameMap.getMapSize());
-		//enemyManager.loadEnemies(gameMap.getObjects("oLayer1"));
-		
 		
 		#if debug
 			textCoord.x = HXP.width - 64;
