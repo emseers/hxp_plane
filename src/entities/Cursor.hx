6959963c0f153addc91071083bc373c003d25447
boarnoah <adipamihi@gmail.com>
2016-01-04 19:38:33 -0500

    Cursor for hitbox etc...

---
+++ src/entities/Cursor.hx
@@ -0,0 +1,19 @@
+package entities;
+import com.haxepunk.Entity;
+
+/**
+ * ...
+ * @author Emseers
+ */
+class Cursor extends Entity{
+
+	public function new() {
+		super();
+	}
+	/*
+	override public function update(){
+		x = scene.mouseX;
+		y = scene.mouseY;
+	}
+	*/
+}
